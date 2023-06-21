65d666d7a6678cd06ab534f71c1aef262d7e03dd
elintendo <elintendo@yandex.ru>
2023-06-21 13:24:05 +0300

    almost done?

--- 1_hw/solution.c
+++ 1_hw/solution.c
@@ -1,8 +1,9 @@
-#include "libcoro.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+
+#include "libcoro.h"
 
 #define FILE_SIZE 10000
 
@@ -11,19 +12,14 @@ void merge(int arr[], int l, int m, int r) {
   int n1 = m - l + 1;
   int n2 = r - m;
 
-  /* create temp arrays */
   int L[n1], R[n2];
 
-  /* Copy data to temp arrays L[] and R[] */
-  for (i = 0; i < n1; i++)
-    L[i] = arr[l + i];
-  for (j = 0; j < n2; j++)
-    R[j] = arr[m + 1 + j];
+  for (i = 0; i < n1; i++) L[i] = arr[l + i];
+  for (j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
 
-  /* Merge the temp arrays back into arr[l..r]*/
-  i = 0; // Initial index of first subarray
-  j = 0; // Initial index of second subarray
-  k = l; // Initial index of merged subarray
+  i = 0;
+  j = 0;
+  k = l;
   while (i < n1 && j < n2) {
     if (L[i] <= R[j]) {
       arr[k] = L[i];
@@ -35,16 +31,12 @@ void merge(int arr[], int l, int m, int r) {
     k++;
   }
 
-  /* Copy the remaining elements of L[], if there
-     are any */
   while (i < n1) {
     arr[k] = L[i];
     i++;
     k++;
   }
 
-  /* Copy the remaining elements of R[], if there
-     are any */
   while (j < n2) {
     arr[k] = R[j];
     j++;
@@ -52,15 +44,10 @@ void merge(int arr[], int l, int m, int r) {
   }
 }
 
-/* l is for left index and r is the right index of the
-   sub-array of arr to be sorted */
 void mergeSort(int arr[], int l, int r) {
   if (l < r) {
-    // Same as (l+r)/2, but avoids overflow for
-    // large l and h
     int m = l + (r - l) / 2;
 
-    // Sort first and second halves
     mergeSort(arr, l, m);
     coro_yield();
     mergeSort(arr, m + 1, r);
@@ -70,47 +57,38 @@ void mergeSort(int arr[], int l, int r) {
 }
 
 void mergeArrays(int arr1[], int arr2[], int n1, int n2, int arr3[]) {
-    int i = 0, j = 0, k = 0;
+  int i = 0, j = 0, k = 0;
 
-    while (i < n1 && j < n2) {
-      if (arr1[i] < arr2[j])
-        arr3[k++] = arr1[i++];
-      else
-        arr3[k++] = arr2[j++];
-    }
-
-    while (i < n1)
+  while (i < n1 && j < n2) {
+    if (arr1[i] < arr2[j])
       arr3[k++] = arr1[i++];
-
-    while (j < n2)
+    else
       arr3[k++] = arr2[j++];
+  }
+
+  while (i < n1) arr3[k++] = arr1[i++];
+
+  while (j < n2) arr3[k++] = arr2[j++];
 }
 
-struct func_arg {
+typedef struct func_arg {
   int *arrToSort;
-  FILE *fileToSort;
+  char *fileToSort;
   struct timespec *start;
-};
+} Func_arg;
 
 static int coroutine_func_f(void *context) {
   //   struct coro *this = coro_this();
   struct func_arg *fa = (struct func_arg *)context;
-  // FILE *fp = fa->fileToSort;
-  // int *numArr = fa->arrToSort;
+  FILE *fp = fopen(fa->fileToSort, "r");
 
   for (int i = 0; i < FILE_SIZE; i++) {
-    fscanf(fa->fileToSort, "%d ", &(fa->arrToSort)[i]);
+    fscanf(fp, "%d ", &(fa->arrToSort)[i]);
   }
 
   mergeSort(fa->arrToSort, 0, FILE_SIZE - 1);
 
-  for (int i = 0; i < FILE_SIZE; i++) {
-    printf("%d ", fa->arrToSort[i]);
-  }
-
-
-  // if (fclose(fa->fileToSort)) { printf("error closing file."); exit(-1); }
-
+  fclose(fp);
   return 0;
 }
 
@@ -132,17 +110,12 @@ int main(int argc, char **argv) {
 
   coro_sched_init();
   int **arrs = (int **)calloc((argc - 2), sizeof(int *));
+  struct func_arg *fa = malloc((argc - 2) * sizeof(struct func_arg));
 
   for (int i = 0; i < argc - 2; ++i) {
-    FILE *fp = fopen(argv[i + 2], "r");
-    if (!fp) {
-      perror("File opening failed");
-      return EXIT_FAILURE;
-    }
-
     *(arrs + i) = (int *)calloc(FILE_SIZE, sizeof(int));
-    struct func_arg fa = {*(arrs + i), fp, &start};
-    coro_new(coroutine_func_f, &fa);
+    fa[i] = (Func_arg){*(arrs + i), argv[i + 2], &start};
+    coro_new(coroutine_func_f, &(fa[i]));
   }
 
   struct coro *c;
@@ -151,37 +124,30 @@ int main(int argc, char **argv) {
     coro_delete(c);
   }
 
-  for(int i = 0; i < FILE_SIZE; i++) {
-    printf("%d ", (*arrs)[i]);
+  for (int i = 0; i < argc - 2 - 1; ++i) {
+    int *ans = malloc(FILE_SIZE * (i + 2) * sizeof(int));
+    mergeArrays(*(arrs + i), *(arrs + i + 1), FILE_SIZE * (i + 1), FILE_SIZE,
+                ans);
+    free(*(arrs + i));
+    free(*(arrs + i + 1));
+    *(arrs + i + 1) = ans;
   }
 
-  // for(int i = 0; i < argc - 2 - 1; ++i) {
-  //   int* ans = malloc(FILE_SIZE * (i + 2) * sizeof(int));
-  //   mergeArrays(*(arrs + i), *(arrs + i + 1), FILE_SIZE, FILE_SIZE, ans);
-  //   free(*(arrs + i));
-  //   free(*(arrs + i + 1));
-  //   *(arrs + i + 1) = ans;
-  // }
-
-  // for (int i = 0; i < argc - 2; ++i) {
-  //   free(*(arrs + i));
-  // }
   FILE *file = fopen("output.txt", "w");
   if (file == NULL) {
-      printf("Error opening the file.\n");
-      return 1;
+    printf("Error opening the file.\n");
+    return 1;
   }
-  
-  for (int i = 0; i < FILE_SIZE*(argc-2); i++) {
-      fprintf(file, "%d\n", arrs[argc-2-1][i]);
-  }
-  
-  fclose(file);
 
+  for (int i = 0; i < FILE_SIZE * (argc - 2); i++) {
+    fprintf(file, "%d\n", arrs[argc - 2 - 1][i]);
+  }
+
+  fclose(file);
 
   free(arrs[argc - 2 - 1]);
   free(arrs);
-
+  free(fa);
 
   clock_gettime(CLOCK_MONOTONIC, &end);
   elapsed_sec = end.tv_sec - start.tv_sec;
@@ -190,7 +156,8 @@ int main(int argc, char **argv) {
     elapsed_sec--;
     elapsed_nsec += 1000000000;
   }
-  printf("Total elapsed time: %lld seconds %lld milliseconds\n", elapsed_sec, elapsed_nsec/1000000);
+  printf("Total elapsed time: %lld seconds %lld milliseconds\n", elapsed_sec,
+         elapsed_nsec / 1000000);
 
   return 0;
 }
